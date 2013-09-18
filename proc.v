adfbbe01877fff805071df3f12ad5aeca51c6791
Adam Wilford <canadianmoose8957@gmail.com>
2013-09-18 14:40:07 -0400

    Added signals to always block. Signals are turned on inside of case statements keyed off of the opcode.

--- proc.v
+++ proc.v
@@ -4,10 +4,15 @@ module proc (DIN, Resetn, Clock, Run, Done, BusWires);
 	output Done;
 	output [8:0] BusWires;
 	parameter T0 = 2’b00, T1 = 2’b01, T2 = 2’b10, T3 = 2’b11;
+	parameter mv = 2’b00, mvi = 2’b01, add = 2’b10, sub = 2’b11;
 	//declare variables
+	reg [1:0] Tstep_Q;
+	reg [1:0] Tstep_D;
+	wire [2:0] I;
 	assign I = IR[1:3];
 	dec3to8 decX (IR[4:6], 1’b1, Xreg);
 	dec3to8 decY (IR[7:9], 1’b1, Yreg);
+	wire RYout, RYin, RXout, RXin, Ain, Gin, Gout, AddSub;
 	// Control FSM state table
     always @(Tstep_Q, Run, Done)
     begin
@@ -43,15 +48,55 @@ module proc (DIN, Resetn, Clock, Run, Done, BusWires);
 			end
 		T1: //deﬁne signals in time step 1
 			case (I)
-			: : :
+				mv: 
+				begin
+					RYout = 1;
+					RXin = 1;
+					Done = 1;
+				end
+				mvi:
+				begin
+					DINout = 1;
+					RXin = 1;
+					Done = 1;
+				end
+				add:
+				begin
+					RXout = 1;
+					Ain = 1;
+				end
+				sub:
+				begin
+					RXout = 1;
+					Ain = 1;
+				end
 			endcase
 		T2: //deﬁne signals in time step 2
 			case (I)
-			: : :
+				add:
+				begin
+					RYout = 1;
+					Gin = 1;
+				end
+				sub:
+				begin
+					RYout = 1;
+					Gin = 1;
+					AddSub = 1;
 			endcase
 		T3: //deﬁne signals in time step 3
 			case (I)
-			: : :
+				add:
+				begin
+					Gout = 1;
+					RXin = 1;
+					Done = 1;
+				end
+				sub:
+				begin
+					Gout = 1;
+					RXin = 1;
+					Done = 1;
 			endcase
 		endcase
 	end
