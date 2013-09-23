dc8eff8e5c239fd7b0e7d236ea10a1fc73c245b9
Adam Wilford <canadianmoose8957@gmail.com>
2013-09-23 13:47:17 -0400

    Added module instantiations to mem.v. Connected counter, memory, and proc modules.

--- mem.v
+++ mem.v
@@ -4,6 +4,13 @@ module mem(mclock, pclock, resetn, run, done, bus);
 	output done;
 	output [8:0] bus;
 	
+	wire [5:0] n;
+	wire [8:0] data;
 	
+	counter count(mclock, resetn, n);
+	
+	memory memory_control(n, mclock, data);
+	
+	proc processor(data, resetn, pclock, run, done, bus);
 	
 endmodule
