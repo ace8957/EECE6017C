bcb7a40251f50a58313cf99f251c2068e292e238
astephens <a.stephens4@gmail.com>
2013-09-29 20:12:00 -0400

    Merged changes

--- proc.v
+++ proc.v
@@ -137,7 +137,6 @@ module proc (DIN, Resetn, Clock, Run, DOUT, ADDR, W);
 		DOUTin <= 0;
 		W_D <= 0;
 		PCincr <= 0;
-		//reg IRin, DINout, RYout, RYin, RXout, RXin, Ain, Gin, Gout, AddSub;
 		case (Tstep_Q)
 		T0: // store DIN in IR in time step 0
 			begin
