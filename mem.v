6e13e52fb88e190f2f9510c4fd4f95a2e144180b
Adam Wilford <canadianmoose8957@gmail.com>
2013-09-30 19:23:50 -0400

    Added switch inputs and module so that switches can be loaded.

--- mem.v
+++ mem.v
@@ -26,24 +26,28 @@
  * seg2_out - 7-segment display 2 output
  * seg3_out - 7-segment display 3 output
  */
-module mem(clock, resetn, run, address, led_output_wires, seg0_out,
+module mem(clock, resetn, run, address, switches, led_output_wires, seg0_out,
 			seg1_out, seg2_out, seg3_out, data_out, data_in);
 	
 	input clock, resetn, run;
+	input [8:0] switches;
 	output [8:0] address;//address connection from memory to proc
 	output [8:0] led_output_wires;
 	output [8:0] seg0_out, seg1_out, seg2_out, seg3_out;
 	
 	/*wire*/ output [8:0] data_out;//data wire between memory and proc transfer
-	/*wire*/ output [8:0] data_in;//data wire between memory and proc transfer
+	/*wire*/ output reg[8:0] data_in;//data wire between memory and proc transfer
 	wire a7, a8;
 	wire W;
 
 	wire led_write;
 	wire mem_write;
 	wire seg_write;
+	wire load_switches;
 	
 	wire [3:0] segSelect;//the first 3 bits of the address will be the selector for which display
+	wire [8:0] switches_value;
+	wire [8:0] memory_output;
 	
 	assign a7 = address[7];
 	assign a8 = address[8];
@@ -51,26 +55,33 @@ module mem(clock, resetn, run, address, led_output_wires, seg0_out,
 	 * 00 = memory write
 	 * 01 = led write
 	 * 10 = 7 segment write
+	 * 11 = load from switches
 	 */
 	assign seg_write = (W & ~(a7 | ~a8));
 	assign led_write = (W & ~(~a7 | a8));
 	assign mem_write = (W & ~(a7 | a8));
-	/*
-	always begin
-		led_write = (W & ~(~a7 | a8));
-		mem_write = (W & ~(a7 | a8));
-		seg_write = (W & ~(a7 | ~a8));
-	end
-	*/
+	assign load_switches = ~(~a7 | ~a8);
+
 	assign segSelect = {address[3], address[2], address[1], address[0]};
+	
+	always @(*) begin
+		if(load_switches == 1'b1) begin
+			data_in <= switches_value;
+		end
+		else begin
+			data_in <= memory_output;
+		end
+	end
 	
 	regn led_reg(data_out, led_write, clock, led_output_wires); 
 	
-	enhanced_mem memory_control(address[6:0], clock, data_out, mem_write, data_in);// the module for memory
+	enhanced_mem memory_control(address[6:0], clock, data_out, mem_write, memory_output);// the module for memory
 	
 	proc processor(data_in, resetn, clock, run, data_out, address, W);// the module for the processor
 	
 	seg7_scroll seven_seg_display(segSelect, data_out, seg_write, clock, seg0_out,
 									seg1_out, seg2_out, seg3_out);
+									
+	switch_load switches_reg(clock, switches, switches_value);
 	
 endmodule
